c1d99a1976a23a70eefc8daad1478b578885fd14
Benatovich <sambenatovich@gmail.com>
2022-05-09 13:36:11 -0400

    app.jsx should be set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Details from './views/Details'
+import List from './views/List'
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path='/characters/:id'>
+          <Details />
+        </Route>
+        <Route path='/'>
+          <List />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
